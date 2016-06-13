7f8207d3aa9abebd6cd483dcfbf398b5baa1a37a
jingwei <jingwei@ldomotors.com>
2016-06-13 18:41:46 +0800

    验证定时器嵌套中断

---
+++ headers/timer.h
@@ -0,0 +1,13 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#include "DSP2803x_Device.h"
+#include "DSP2803x_Examples.h"
+
+__interrupt void TIM0_ISR(void);
+__interrupt void TIM1_ISR(void);
+
+void Timer_Init(void);
+
+
+#endif
---
+++ source/timer.c
@@ -0,0 +1,55 @@
+#include "timer.h"
+
+void Timer_Init(void)
+{
+	InitCpuTimers();
+	ConfigCpuTimer(&CpuTimer0, 60, 1000000);
+	ConfigCpuTimer(&CpuTimer1, 60, 500000);		//let timer1 into isr at frist
+	CpuTimer0Regs.TCR.all = 0x4000;
+	CpuTimer1Regs.TCR.all = 0x4000;
+	IER |= M_INT1;
+	IER |= M_INT13;
+	PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
+
+	EALLOW;
+	PieVectTable.TINT0 = &TIM0_ISR;
+	PieVectTable.TINT1 = &TIM1_ISR;
+	EDIS;
+
+	EINT;
+	ERTM;
+}
+
+
+__interrupt void TIM0_ISR(void)
+{
+	GpioDataRegs.GPATOGGLE.bit.GPIO7 = 1;
+	PieCtrlRegs.PIEACK.all = 1;
+}
+
+
+__interrupt void TIM1_ISR(void)
+{
+
+ 	//test interrupt nesting
+	//	int i;
+	short TempPIE = PieCtrlRegs.PIEIER1.all;
+
+	IER |= 0x0001;
+	IER &= 0x0001;
+
+	PieCtrlRegs.PIEIER1.all &= 0x0040;		//timer0 interrupt
+	PieCtrlRegs.PIEACK.all = 0xFFFF;
+
+	asm("		NOP");
+	EINT;
+
+//	for(i=0;i<30000;i++){}
+
+	while(1){}
+
+//	DINT;
+//	PieCtrlRegs.PIEIER1.all = TempPIE;
+
+//	PieCtrlRegs.PIEACK.all = 1;
+}
